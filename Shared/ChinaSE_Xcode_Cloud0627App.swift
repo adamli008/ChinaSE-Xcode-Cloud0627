6a762e153ca37770915eb00b20c0e4a5fc5c8a61
Adam Li <adamli@189.cn>
2022-06-27 10:59:37 +0800

    Initial Commit

---
+++ Shared/ChinaSE_Xcode_Cloud0627App.swift
@@ -0,0 +1,17 @@
+//
+//  ChinaSE_Xcode_Cloud0627App.swift
+//  Shared
+//
+//  Created by adam li on 2022/6/27.
+//
+
+import SwiftUI
+
+@main
+struct ChinaSE_Xcode_Cloud0627App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
