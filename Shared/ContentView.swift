39070140bb22ff7afb3015a0b5a8b5dd026af9bd
Adam Li <adamli@189.cn>
2022-06-27 12:14:16 +0800

    Add text

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, Earth!")
             .padding()
-        Text("Hello, Apple!")
+        Text("Hello, Apple!！！")
             .padding()
     }
 }
